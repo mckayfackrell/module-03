2804967b7ba160938df3af957a580c5bbf8ba285
McKay Fackrell <110206514+mckayfackrell@users.noreply.github.com>
2022-10-01 20:00:09 -0600

    added window prompt for reuqesting password length with number alert

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,23 @@
 // Assignment code here
+function generatePassword(){
 
+  var userInput = window.prompt("How long do you want the password? 8-128 characters long")
+  
+  var passwordLength = parseInt(userInput)
+
+  if(isNaN(passwordLength)){
+    window.alert("Try again, that is not a number!")
+
+  }
+
+  // if(isNaN(passwordLength < 8)){
+  //   window.alert("Please enter a password longer than 8 characters.")
+  // }
+
+
+
+
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
