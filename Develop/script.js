8964cc2d5d840ac725ff20d88bd8f61b9c880df3
McKay Fackrell <110206514+mckayfackrell@users.noreply.github.com>
2022-10-01 20:25:54 -0600

    added var for each criteria with window.confirm prompts for password

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,25 @@
 // Assignment code here
 function generatePassword(){
 
-  var userInput = window.prompt("How long do you want the password? 8-128 characters long")
+  var userInput = window.prompt("How long do you want the password?")
   
   var passwordLength = parseInt(userInput)
 
   if(isNaN(passwordLength)){
     window.alert("Try again, that is not a number!")
-
+    return
   }
+
+  if (passwordLength < 8 || passwordLength > 128){
+    window.prompt("Choose a number between 8-128")
+    return
+  }
+
+var selectLowercase = window.confirm("Would you like to have lowercase letters in your password?")
+var selectUppercase = window.confirm("Would you like to have uppercase letters in your password?")
+var selectNumbers = window.confirm("Would you like to have numbers in your password?")
+var selectSymbols = window.confirm("Would you like to have symbols in your password?")
+
 
   // if(isNaN(passwordLength < 8)){
   //   window.alert("Please enter a password longer than 8 characters.")
